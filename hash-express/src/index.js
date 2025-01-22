98a8b38f216820f9f16d405e668840177f9ccd88
ZeeKotomori <zikriaf23@gmail.com>
2025-01-22 07:40:38 +0700

    update anyar ges, jwt

--- hash-express/src/index.js
+++ hash-express/src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const userRoutes = require('./routes/userRoute.js');
+const authRoutes = require('./routes/authRoute.js');
 require('dotenv').config();
 
 app.use(express.json());
 
+app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.get('/', (req, res) => {
     res.send('Hello World!');
--- hash-express/src/routes/userRoute.js
+++ hash-express/src/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController.js');
+const jwtMiddleware = require('../middlewares/jwtMiddleware.js');
 
 const router = express.Router();
 
 // Define routes for user operations
-router.get('/', userController.getAllUsers);
+router.get('/', jwtMiddleware, userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
 router.put('/:id', userController.updateUser);
